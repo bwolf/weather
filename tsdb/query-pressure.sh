8a8282851db1ac11723b25de45b5448da27b9d1c
Marcus Geiger <web0c@antbear.org>
2016-02-23 09:53:03 +0100

    Remove tsdb sketch code, update readme.

--- tsdb/query-pressure.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-curl -G 'http://localhost:8086/query?pretty=true' \
-     --data-urlencode "db=weather" \
-     --data-urlencode "q=SELECT value FROM temp WHERE station='1';"
--- tsdb/query-rh_true.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-curl -G 'http://localhost:8086/query?pretty=true' \
-     --data-urlencode "db=weather" \
-     --data-urlencode "q=SELECT value FROM rh_true WHERE station='1';"
